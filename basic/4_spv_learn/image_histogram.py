96f9b5c79a1b02cd0baf9bac33219a59ccaab1c2
knagara <kukkusonia@gmail.com>
2014-06-25 15:36:50 +0900

    svm

---
+++ basic/4_spv_learn/image_histogram.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import cv2
+import pylab as plt
+
+#/Users/Keita/Documents/temp/Nature/02.jpg
+#/Users/Keita/Documents/temp/City/25.jpg
+#/Users/Keita/Documents/temp/Rural/1.jpg
+#/Users/Keita/Documents/temp/Machi/1.jpg
+
+
+if __name__ == '__main__':
+
+    # 画像取得
+    #im = cv2.imread("/Users/Keita/Documents/temp/Nature/11.jpg")
+    #im = cv2.imread("/Users/Keita/Documents/temp/City/25.jpg")
+    im = cv2.imread("/Users/Keita/Documents/temp/Rural/2.jpg")
+    #im = cv2.imread("/Users/Keita/Documents/temp/Machi/1.jpg")
+
+    graph = plt.figure()
+
+    # チャネルB(青)のヒストグラム
+    graph.add_subplot(311)
+    plt.hist(im[:,:,0].ravel(), 256, range=(0, 255), fc='b')
+    plt.xlim(0,255)
+
+    # チャネルG(緑)のヒストグラム
+    graph.add_subplot(312)
+    plt.hist(im[:,:,1].ravel(), 256, range=(0, 255), fc='g')
+    plt.xlim(0,255)
+
+    # チャネルR(赤)のヒストグラム
+    graph.add_subplot(313)
+    plt.hist(im[:,:,2].ravel(), 256, range=(0, 255), fc='r')
+    plt.xlim(0,255)
+
+    # ヒストグラム表示
+    plt.show()
--- basic/4_spv_learn/svm_image.py
+++ basic/4_spv_learn/svm_image.py
@@ -3,12 +3,12 @@ import cv2
 import pylab as plt
 import numpy as np
 import glob
-from sklearn import svm
+from sklearn import svm, grid_search
 
 def getHist(file_):
 	img = cv2.imread(file_,cv2.IMREAD_COLOR)
-	hist_item = cv2.calcHist([img],[ch],None,[100],[0,255])
-	cv2.normalize(hist_item,hist_item,0,255,cv2.NORM_MINMAX)
+	hist_item = cv2.calcHist([img],[1],None,[10],[0,255])
+	cv2.normalize(hist_item,hist_item,0,100,cv2.NORM_MINMAX)
 	hist=np.int32(np.around(hist_item))
 	return hist
 
@@ -30,15 +30,41 @@ if __name__ == '__main__':
 	y = []
 
 	#自然 0
-	filelist = glob.glob('/Users/Keita/Documents/temp/Nature/*')
+	filelist = glob.glob('/Users/Keita/Documents/temp/Rural/*')
 	histogram(filelist,0,X,y,ch)
 	#都市 1
-	filelist = glob.glob('/Users/Keita/Documents/temp/City/*')
+	filelist = glob.glob('/Users/Keita/Documents/temp/Machi/*')
 	histogram(filelist,1,X,y,ch)
 
-	clf = svm.SVC(kernel='rbf') #Support Vector Classification(分類)、RBFカーネルを使用
+	#parameters = {'kernel':['rbf'], 'C':np.logspace(-4, 4, 10), 'gamma':np.logspace(-4, 4, 10)}
+	clf = svm.SVC()
+	#clf = grid_search.GridSearchCV(clf, parameters, n_jobs = -1)
 	clf.fit(X, y) #学習
 	#print clf.predict([2,2,4]) #予測
+
+	#0になるはず
+	Z0 = []
+	filelist = glob.glob('/Users/Keita/Documents/temp/Nature/*')
+	for file_ in filelist:
+		hist = getHist(file_)
+		x = []
+		for h in hist:
+			x.append(h[0])
+		Z0.append(clf.predict(x)[0])
+
+	#1になるはず
+	Z1 = []
+	filelist = glob.glob('/Users/Keita/Documents/temp/City/*')
+	for file_ in filelist:
+		hist = getHist(file_)
+		x = []
+		for h in hist:
+			x.append(h[0])
+		Z1.append(clf.predict(x)[0])
+
+	print Z0
+	print Z1
+
 
 
 
